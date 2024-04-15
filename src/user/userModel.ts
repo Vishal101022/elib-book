32d74cc334b0802c25eb532cc14887a1483be01b
Vishal101022 <raivishal939@gmail.com>
2024-04-15 17:59:48 +0530

    Add: user schema

---
+++ src/user/userModel.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+interface User {
+    _id: string;
+    name: string;
+    email: string;
+    password: string;
+}
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema<User>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<User>("User", userSchema);
