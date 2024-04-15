6fe7985a8a097fd123ec70a2a92a9bc605381cdf
Vishal101022 <raivishal939@gmail.com>
2024-04-15 12:51:20 +0530

    Add: database connection

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 require("dotenv").config();
 import app from "./src/app";
+import  connectDB  from "./lib/db";
 
-const startServer = () => {
-  const port = process.env.PORT;
+const startServer = async () => {
+    // database connection
+    await connectDB();
+    const port = process.env.PORT;
   app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
   });
