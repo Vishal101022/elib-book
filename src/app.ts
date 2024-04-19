75d051ffd4e5bf4d35129c8e2b8663e5c73b175a
Vishal101022 <raivishal939@gmail.com>
2024-04-19 23:05:51 +0530

    Add: book api middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import globalError from "./middlewares/globalError";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.get("/", (req, res) => {
     res.json({ message: "welcome to elib" });
 })
 app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
 app.use(globalError);
 export default app;
