c37745f71a6730d75387e48e3712a4deec3b20e4
Vishal101022 <raivishal939@gmail.com>
2024-04-29 20:49:33 +0530

    Update: book schema

--- src/book/bookModel.ts
+++ src/book/bookModel.ts
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
+
 interface Book {
-  _id: string;
   title: string;
-  author: string;
-  publiction_year: number;
+  author: mongoose.Types.ObjectId;
+  publication_year: number;
   genre: string;
   description: string;
-  image: string;
-  url: string;
 }
 
 const bookSchema = new mongoose.Schema<Book>(
   {
     title: { type: String, required: true },
-    author: { type: String, required: true },
-    publiction_year: { type: Number, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    publication_year: { type: Number, required: true },
     genre: { type: String, required: true },
     description: { type: String, required: true },
-    image: { type: String, required: true },
-    url: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.model<Book>("Book", bookSchema)
+export default mongoose.model<Book>("Book", bookSchema);
